7b45707c5ce2fb8e28d00699c4a25e03b25feb1c
jayden-claude <jadensadiqq@gmail.com>
2025-02-25 18:15:39 +0000

    chore: add schema.rb

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 20_240_715_200_615) do
+  create_table 'playlists', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'videos', force: :cascade do |t|
+    t.integer 'playlist_id', null: false
+    t.string 'video_id'
+    t.string 'title'
+    t.text 'description'
+    t.string 'thumbnail_url'
+    t.integer 'views'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['playlist_id'], name: 'index_videos_on_playlist_id'
+  end
+
+  add_foreign_key 'videos', 'playlists'
+end
